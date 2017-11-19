183d7974c03b40d3c88fea0921c992496251f01f
happyyuyu <zhouyh1997@gmail.com>
2017-11-18 23:22:36 -0500

    some skeleton code for kmalloc and kfree

--- kmalloc.c
+++ kmalloc.c
@@ -3,6 +3,9 @@
 // After you are done, you don't need to import it anymore. This is here just for the dummy code on malloc/free.
 #include<stdlib.h>
 
+uint64_t * start = NULL;
+uint64_t MAGIC = 0xabcdef;
+
 void *palloc(uint64_t number) {
 	// TODO:
 	// 1) Allocate 1 frame of memory
@@ -10,10 +13,10 @@ void *palloc(uint64_t number) {
 	// 3) Map the found page number (virtual) to the allocated frame address (not the frame number, but the frame address)
 	// 4) Return the virtual address of the first byte of the allocated frame
 
-	uint64_t frame = allocate_frame(...);
+	uint64_t frame = allocate_frame(number);
 	if (frame == -1) return -1;
-	uint64_t page = vm_locate(...)
-	vm_map(page, frame, /* one page */, 0);
+	uint64_t page = vm_locate(number)
+	vm_map(page, frame, 1, 0);
 	return GET_FIRST_BYTE(frame);
 
 	// Dummy code: you cannot use malloc/free
@@ -39,6 +42,28 @@ void pfree(void* address, uint64_t number) {
 void *kmalloc(uint64_t size) {
 	// TODO:
 	// - Implement a linked list of free chunks using only palloc()
+	if (!start){ //create a new linked list
+
+	}
+	else { //The linked list is already created, so find the next avaliable chunk
+		//with enough space
+		uint64_t * temp = start;
+		uint64_t * prev = NULL;
+		while (*(temp+1) && ((size + 2 * sizeof(uint64_t)) > *temp)){
+			//The first free chunk with enough space, or the last chunk of the page
+			prev = temp;
+			temp = * (temp + 1);
+		}
+		if (size + 2 * sizeof(uint64_t)) <= *temp){
+			//The chunk is large enough
+
+
+		}
+		else{
+			//allocate another chunk
+
+		}
+	}
 	// - Use the first-fit strategy to allocate a chunk
 
 
@@ -50,7 +75,9 @@ void *kmalloc(uint64_t size) {
 void *krealloc(void *address, uint64_t size) {
 	// TODO:
 	//
+
 	// - If the address is becoming smaller, return the last frames that have become unused with vm_unmap() and frame_deallocate()
+
 	// - If the address is becoming bigger, and is possible to allocate new contiguous pages to extend the chunk size,
 	//   allocate new frames and map their pages.
 	// - If the address is becomming bigger, but it is not possible to allocate new contigous pages to extend the chunk size,
@@ -64,6 +91,15 @@ void *krealloc(void *address, uint64_t size) {
 void kfree(void *address) {
 	// TODO:
 	// - Make the space used by the address free
+	uint64_t * first = address - 2;
+	uint64_t former_size = *first;
+	uint64_t * next = (uint64_t*)((char*)adress + former_size - 2 * sizeof(uint64_t));
+	if(*(next+1) == MAGIC){
+		//
+
+	}
+
+
 	// - Return any frames that have become unused with vm_unmap() and frame_deallocate()
 
 	// Dummy code: you cannot use malloc/free
